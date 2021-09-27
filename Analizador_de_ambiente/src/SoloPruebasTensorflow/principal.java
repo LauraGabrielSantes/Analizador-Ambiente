e3040b035b1b5a817031c60c2383246fc5ffbb06
Laura-Gabriel <laura_gabsant@hotmail.com>
2021-09-26 23:39:53 -0500

    cambio

--- Analizador_de_ambiente/src/SoloPruebasTensorflow/principal.java
+++ Analizador_de_ambiente/src/SoloPruebasTensorflow/principal.java
@@ -1,29 +1,10 @@
 package SoloPruebasTensorflow;
 
-import org.tensorflow.Graph;
-import org.tensorflow.Session;
-import org.tensorflow.Tensor;
-import org.tensorflow.TensorFlow;
-
 public class principal {
-  public static void main(String[] args) throws Exception {
-    try (Graph g = new Graph()) {
-      final String value = "Hello from " + TensorFlow.version();
 
-      // Construct the computation graph with a single operation, a constant
-      // named "MyConst" with a value "value".
-      try (Tensor t = Tensor.create(value.getBytes("UTF-8"))) {
-        // The Java API doesn't yet include convenience functions for adding operations.
-        g.opBuilder("Const", "MyConst").setAttr("dtype", t.dataType()).setAttr("value", t).build();
-      }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
 
-      // Execute the "MyConst" operation in a Session.
-      try (Session s = new Session(g);
-          // Generally, there may be multiple output tensors,
-          // all of them must be closed to prevent resource leaks.
-          Tensor output = s.runner().fetch("MyConst").run().get(0)) {
-        System.out.println(new String(output.bytesValue(), "UTF-8"));
-      }
-    }
-  }
 }
